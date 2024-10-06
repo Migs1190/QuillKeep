7848edf6241b09d8cfabf10886f25f0a8243c324
Migs1190 <ahmad1372002@gmail.com>
2024-10-06 16:46:32 +0300

    chore: add color constants for light and dark themes

---
+++ constants/Colors.js
@@ -0,0 +1,37 @@
+// const tint = "#FE0000";
+const tint = "#B17457";
+
+export default {
+	tint,
+	tintDarker: "#803D3B",
+	tintLighter: "#f5b7a6",
+	active: "#fff",
+	light: {
+		heading: "#4A4947",
+		text: "#374151",
+		link: tint,
+		background: "#FAF7F0",
+		componentBackground: "#FAF7F0",
+		componentBackgroundSecondary: "#f5f5f5",
+		border: "#6F4E37",
+		tabIconDefault: "#6c6c6c",
+		tabIconSelected: tint,
+		iconDefault: "#4A4947",
+		iconFirst: "#000",
+		active: "#fff",
+	},
+	dark: {
+		heading: tint,
+		text: "#fff",
+		link: tint,
+		background: "#161A1B", //Wood Smoke
+		componentBackground: "#2a2a2a", //Mine Shaft
+		componentBackgroundSecondary: "#1C2022", //Shark
+		border: "#D8D2C2",
+		tabIconDefault: "#6e6262",
+		tabIconSelected: tint,
+		iconDefault: "#A8A196",
+		iconFirst: "#fff",
+		active: "#fff",
+	},
+};
